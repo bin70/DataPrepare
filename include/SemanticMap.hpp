c8f5f11d2b909b81f1e9696d48bf6d769526e634
bin70 <jinbin_tan@icloud.com>
2020-08-14 09:45:40 +0800

    use TXTReader to load semantic map

--- include/SemanticMap.hpp
+++ include/SemanticMap.hpp
@@ -1,86 +1,76 @@
 #pragma once
-#include <map>
-#include <pcl/io/pcd_io.h>
-#include <point_cloud/common.hpp>
+#include <constant/label.h>
 #include <utils/common.hpp>
-#include <io/FileOperator.hpp>
-
-enum LABEL_TYPE
-{
-    CEILING, FLOOR, WALL, CLUTTER
-};
+#include <point_cloud/TXTReader.hpp>
 
 class SemanticMap
 {
+    using string = std::string;
+
 public:
-    std::map<std::string, int> label_map = {
-        {"ceiling", CEILING},
-        {"floor",   FLOOR},
-        {"wall",    WALL},
-        {"clutter", CLUTTER}
+    std::map<int, string> label_name = {
+        {CEILING, "ceiling"},
+        {FLOOR, "floor"},
+        {WALL,  "wall"},
+        {CLUTTER, "clutter"}
     };
 
 public:
-    SemanticMap(PointCloud::Ptr map_cloud, std::string map_folder)
+    SemanticMap(string map_folder, float resolution = 0.3) :
+        _resolution(resolution),
+        _map_folder(map_folder),
+        _cloud(new PointCloud),
+        _map_manager(new MapManager(_resolution))
     {
         std::cout << "Loading semantic map ..." << std::endl;
-        loadMap(map_cloud, map_folder);
-        std::cout << "Load semantic map finished." << std::endl;
+        
+        loadMap();
+
+        std::cout << "Loaded semantic map:" << std::endl
+                  << "\t - resolution = " << _resolution << std::endl
+                  << "\t - size = " << getMapPtr()->points.size() << std::endl;
         pLine();
     }
 
-    bool loadMap(PointCloud::Ptr &map_cloud, std::string rootDir)
+    bool loadMap()
     {
-        if (!openDir(rootDir))
-            return false;
+        reader.openDir(_map_folder);
+        reader.setDataCol(3);
 
-        for (int label = 0; label <= label_map["clutter"]; ++label)
+        for (int label = 0; label < CLUTTER; ++label)
         {
-            std::ifstream fcloud(file_list[label]);
-            if (!fcloud.is_open())
+            reader.readPointCloud(_cloud, label_name[label]);
+            for(auto &p : _cloud->points)
             {
-                std::cout << "打开文件:" << file_list[label] << " 失败" << std::endl;
-                return false;
+                p.curvature = label;
             }
+            
+            // _map_manager->getMapPtr()->points.push_back(p);
 
-            PointType p;
-            while (!fcloud.eof())
-            {
-                fcloud >> p.x;
-
-                if (fcloud.eof())
-                    break;
-
-                fcloud >> p.y >> p.z >> p.intensity;
-                p.curvature = (float)label;
-                map_cloud->points.push_back(p);
-            }
-
-            consoleProgress(100/label_map.size()*(label+1));
+            if(label == 0)
+                _map_manager->UpdateMap(_cloud);
+            else
+                _map_manager->AddFrameToMap(_cloud);
+                
+            consoleProgress(100/(label_name.size()-1)*(label+1));
         }
+
+        //_map_manager->update();
+        _cloud->clear();
         return true;
     }
-
-    bool openDir(std::string rootDir)
+    bool searchPoint(PointType& query, PointType& result)
     {
-        if (access(rootDir.c_str(), 0) != 0)
-            return false;
-
-        DIR *dir = opendir(rootDir.c_str());
-        dirent *ptr;
-        while (ptr = readdir(dir))
-        {
-            if (ptr->d_name[0] == '.')
-                continue;
-
-            std::string filePath = rootDir + "/" + std::string(ptr->d_name);
-            int label = atoi(fop.getFileName(filePath).c_str());
-            file_list[label] = filePath;
-        }
-        return true;
+        return _map_manager->getNearestPoint(query, result);
     }
+
+    const PointCloud::Ptr& getMapPtr(){ return _map_manager->getMapPtr(); }
 
 private:
-    std::map<int, std::string> file_list;
+    TXTReader reader;
     FileOperator fop;
+    float _resolution;
+    string _map_folder;
+    PointCloud::Ptr _cloud;
+    MapManager* _map_manager;
 };
